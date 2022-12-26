ea5ea01e922bd0df2bdf37e562dc198fa8391f7d
Yansa27 <juliansa837@gmail.com>
2022-12-26 20:26:54 +0700

    push update

---
+++ javascript/lterasi.js
@@ -0,0 +1,9 @@
+// menerapkan lterasi
+
+for (let number = 1 ; number <= 100 ; number++) {
+    if (number % 3 === 0) {
+        console.log("kelipatan 3")
+    }else {
+        console.log(number)
+    }
+}
