fff8e3291b10ea269467d4c893072df2c66823e7
Yansa27 <juliansa837@gmail.com>
2022-12-28 00:05:37 +0700

    commit

---
+++ combine2.js
@@ -0,0 +1,6 @@
+// array object for if
+
+const identitas = [
+    {nama : "yansa" , age : 18},
+    
+]
--- javascript/object.js
+++ javascript/object.js
@@ -9,3 +9,13 @@ console.log (indentitas)
 indentitas.umur = 17
 
  console.log (indentitas)
+
+//  object di dalam array 
+
+const mhs = [
+    {name : "yansa" , class : 10},
+    {name : "yansa1" , class : 11}
+]
+
+console.log (mhs[0]);
+console.log (mhs[1].name);
