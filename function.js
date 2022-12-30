bd7f9c1da0e166e361df257d4f54bb0fe9da1b31
Yansa27 <juliansa837@gmail.com>
2022-12-30 23:51:41 +0700

    push

--- function.js
+++ function.js
@@ -30,3 +30,52 @@ const add = (number1 , number2) => {
 }
 
 add (5,7);
+
+// return fuction
+
+const value = (nilai) => {
+    return nilai / 2 ;
+}
+
+const result = value (130) ;
+
+console.log (`Hasil 130 dibagi 2 adallah ${result}`);
+
+// menggunakan nilai return 
+
+const add1 = (check) => {
+    return check % 3 == 0 ;
+}
+if ( add1(123) ) {
+    console.log("multiple 3");
+} else {
+    console.log("Not multiple 3");
+}
+
+
+// latihan akhir mencarir nilai terkecil
+
+number1 = 100
+number2 = 60
+number3 = 20
+number4 = 10
+
+const getmax = (a , b , c , d) => {
+    let max = a ;
+
+    if (max > b ) {
+        max = b;
+    }
+    if (max > c ) {
+        max = c;
+    }
+    if (max > d) {
+        max = d;
+    }
+
+    return max
+};
+
+const max = getmax(number1 , number2 , number3 , number4);
+
+console.log(`Nilai terkecil adalah ${max}`);
