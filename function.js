140e3f24867557955457447240853365fcd9ab4a
Yansa27 <juliansa837@gmail.com>
2022-12-30 22:54:21 +0700

    push

---
+++ function.js
@@ -0,0 +1,24 @@
+// cara nulis function
+
+const objek = function() {
+    console.log ("yansa");
+    console.log ("Hallo");
+}
+
+objek ();
+// cara cepat arrow function 
+
+const iden = () => {
+    console.log ("hallo deck");
+    console.log ("hallo guys");
+}
+
+iden ();
+
+// argument 
+const greet = (name) => {
+    console.log (`Halo ${name}`);
+
+}
+
+greet("Guru Domba");
