83b18380e3baa94bfc05de949cb0d4356a2a1301
Yansa27 <juliansa837@gmail.com>
2023-01-01 23:58:31 +0700

    push

--- manipulation.js
+++ manipulation.js
@@ -1,3 +1,11 @@
-function popup() {
-    alert("hello world!");
+document.getElementById("blue").onclick = function () {
+    document.getElementById("image").src = "/image/blue.jpg";
+}
+
+document.getElementById("green").onclick = function () {
+    document.getElementById("image").src = "/image/green.jpg";
+}
+
+document.getElementById("red").onclick = function () {
+    document.getElementById("image").src = "/image/red.jpg";
 }
